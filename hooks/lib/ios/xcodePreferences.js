62f664806af0964cba5420c61067d4acfe4bc42e
eldadfux <eldad.fux@gmail.com>
2018-12-14 22:52:40 +0200

    Fix for nordnet #131 and #110: Cannot find module '../plugman/platforms/ios'

--- hooks/lib/ios/xcodePreferences.js
+++ hooks/lib/ios/xcodePreferences.js
@@ -133,20 +133,26 @@ function isPbxReferenceAlreadySet(fileReferenceSection, entitlementsRelativeFile
  * @return {Object} projectFile - project file information
  */
 function loadProjectFile() {
-  var platform_ios;
-  var projectFile;
-
-  try {
-    // try pre-5.0 cordova structure
-    platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms')['ios'];
-    projectFile = platform_ios.parseProjectFile(iosPlatformPath());
-  } catch (e) {
-    // let's try cordova 5.0 structure
-    platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms/ios');
-    projectFile = platform_ios.parseProjectFile(iosPlatformPath());
-  }
-
-  return projectFile;
+    var platform_ios;
+    var projectFile;
+    try {
+        // try pre-5.0 cordova structure
+        platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms')['ios'];
+        projectFile = platform_ios.parseProjectFile(iosPlatformPath());
+    } catch (e) {
+        try {
+            // let's try cordova 5.0 structure
+            platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms/ios');
+            projectFile = platform_ios.parse(iosPlatformPath());
+        } catch (e) {
+            // try cordova 7.0 structure
+            var iosPlatformApi = require(path.join(iosPlatformPath(), '/cordova/Api'));
+            var projectFileApi = require(path.join(iosPlatformPath(), '/cordova/lib/projectFile.js'));
+            var locations = (new iosPlatformApi()).locations;
+            projectFile = projectFileApi.parse(locations);
+        }
+    }
+    return projectFile;
 }
 
 /**
